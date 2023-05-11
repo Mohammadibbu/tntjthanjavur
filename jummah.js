7544de43a5e8137bb863fd15fc391aa53d55af28
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-05-12 01:02:48 +0530

    Update jummah.js

--- jummah.js
+++ jummah.js
@@ -129,3 +129,17 @@ function removeData(key,uservalue){
         }
 
 }
+$(window).on('scroll',function(){
+            if($(window).scrollTop()){
+                $('nav').addClass('black');
+            }
+            else{
+                $('nav').removeClass('black');
+            }
+        })
+  /*menu button onclick function*/         $(document).ready(function(){
+                $('.menu h4').click(function(){
+                    $("nav ul").toggleClass("active")
+            })
+            })
+ 
